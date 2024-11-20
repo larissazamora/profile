5420cc92972831ddfb76bccb792890771a3e2bb4
larissazamora <larissazamora@hotmail.com>
2024-11-20 14:54:24 -0300

    alterações de css

--- java-script.js
+++ java-script.js
@@ -1,28 +1,57 @@
-function validarFormulario () {
-    const nome = document.getElementById('nome').value;
-    console.log(nome);
-    const email = document.getElementById('email').value;
-    const assunto = document.getElementById('assunto').value;
-    const mensagem = document.getElementById('mensagem').value;
+function validarFormulario(event) {
+  //event.preventDefault()
+  const nome = document.getElementById('nome').value;
+  console.log(nome);
+  const email = document.getElementById('email').value;
+  const assunto = document.getElementById('assunto').value;
+  const mensagem = document.getElementById('mensagem').value;
 
-    const emailRegex = /\S+@\S+\.\S+/;
-    if (nome === '') {
-        document.getElementById('nomeError').textContent = "Por favor, preencha o campo nome.";
-        return false;
-      } else {
-        document.getElementById('nomeError').textContent = "";
-        
-      }
+  const emailRegex = /\S+@\S+\.\S+/;
 
-      if (!emailRegex.test(email)) {
-        document.getElementById('emailError').textContent = "Por favor, insira um email válido.";
-        return false;
-      } else {
-        document.getElementById('emailError').textContent = "";
-      }
-    }
-    
-   
+  if (nome === '') {
+    document.getElementById('nomeError').textContent = "Por favor, preencha o campo nome.";
+  } else {
+    document.getElementById('nomeError').textContent = "";
+
+  }
+
+  if (!emailRegex.test(email)) {
+    document.getElementById('emailError').textContent = "Por favor, insira um email válido.";
+  } else {
+    document.getElementById('emailError').textContent = "";
+  }
+
+
+  if (assunto === '') {
+    document.getElementById('assuntoError').textContent = "Por favor, preencha o campo assunto.";
+  } else {
+    document.getElementById('assuntoError').textContent = "";
+  }
+}
+
+if (mensagem === '') {
+  document.getElementById('mensagemError').textContent = "Por favor, digite sua mensagem.";
+} else {
+  document.getElementById('mensagemError').textContent = "";
+}
+
+
+const formulario = document.getElementById('meuFormulario');
+const mensagemSucesso = document.getElementById('mensagemSucesso');
+
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+    mensagemSucesso.style.display = 'block';
+
+    // Ocultar a mensagem após 3 segundos
+    setTimeout(() => {
+        mensagemSucesso.style.display = 'none';
+    }, 3000);
+});
+
+
+
+
 
 
 
@@ -38,6 +67,6 @@ const regex = /\S+@\S+\.\S+/;
 console.log(regex.test(email));
 
 function validarEmail(email) {
-    const regex = /\S+@\S+\.\S+/;
-    return regex.test(email);
+  const regex = /\S+@\S+\.\S+/;
+  return regex.test(email);
 }
